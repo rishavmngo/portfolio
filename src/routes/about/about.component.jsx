e068f4f341d8d541f0f9ed637225c28f099d2a3e
rishavmngo <rishavinmngo@gmail.com>
2023-02-07 22:17:51 +0530

    working on about me section

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -1,6 +1,21 @@
 import './about.style.css'
 const About = () => {
-  return <div className='about-component'>About</div>
+  return (
+    <div className='about-component'>
+      <h2 className='about-component-main-title'>
+        About <span className='theme-color'>me</span>
+      </h2>
+      <div className='about-me-container'>
+        <div className='about-me-desp'>
+          Hello, My name is Rishav Raj. I built apps for web. I made many{' '}
+          <span className='theme-color'>web apps </span>
+          and few <span className='theme-color'>command line tools</span>. I
+          love exploring computers either software or underling hardware.
+        </div>
+        <div className='about-me-pic'></div>
+      </div>
+    </div>
+  )
 }
 
 export default About
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,27 +3,29 @@ import './home.style.css'
 const Home = () => {
   return (
     <div className='home-component'>
-      <div className='navigation'>
-        <span className='navigation-link'>work</span>
-        <span className='divider'></span>
-        <span className='navigation-link'>blog</span>
-      </div>
-      <div className='title'>
-        <div className='intro'>I am</div>
-        <div className='name'>
-          <span className='firstName'>Rishav </span>
-          <span className='lastName'>
-            Raj
-            <div className='circle'></div>
-          </span>
+      <div className='hero-container'>
+        <div className='navigation'>
+          <span className='navigation-link'>work</span>
+          <span className='divider'></span>
+          <span className='navigation-link'>blog</span>
         </div>
-      </div>
-      <div className='hero-line'>
-        <div>
-          <span className='hero-line-color'>Developer</span> on duty
+        <div className='title'>
+          <div className='intro'>I am</div>
+          <div className='name'>
+            <span className='firstName'>Rishav </span>
+            <span className='lastName'>
+              Raj
+              <div className='circle'></div>
+            </span>
+          </div>
         </div>
-        <div>
-          love making <span className='hero-line-color'>web apps</span>
+        <div className='hero-line'>
+          <div>
+            <span className='hero-line-color'>Developer</span> on duty
+          </div>
+          <div>
+            love making <span className='hero-line-color'>web apps</span>
+          </div>
         </div>
       </div>
       <About />
