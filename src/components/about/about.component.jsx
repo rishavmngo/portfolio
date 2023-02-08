dc18c3d6a05275f013b7a4527054306a54994d11
rishavmngo <rishavinmngo@gmail.com>
2023-02-08 22:58:22 +0530

    fix spacing and add hover effect for profile image

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -8,10 +8,9 @@ const About = () => {
           <div className='section-heading'>
             About <span className='highlight'>me</span>
           </div>
-
           <div className='about-content-grid-desp'>
             Hello, My name is Rishav Raj. I built apps for web. I made many
-            <span className='highlight'>web apps </span>
+            <span className='highlight'> web apps </span>
             and few <span className='highlight'>command line tools</span>. I
             love exploring computers either software or underling hardware.
           </div>
